ee78f7fc2bad241fa0e20ac5213d4cf5107e49a1
tw_luo <2505412775@qq.com>
2025-02-06 11:05:34 +0800

    finish c++

--- learning-cxx/exercises/30_std_unique_ptr/main.cpp
+++ learning-cxx/exercises/30_std_unique_ptr/main.cpp
@@ -49,19 +49,26 @@ int main(int argc, char **argv) {
 
     // ---- 不要修改以上代码 ----
 
+    // for (auto i = 0; i < 3; ++i) {
+    //     for (auto j = 0; j < problems[i].size(); ++j) {
+    //         std::cout << problems[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
     std::vector<const char *> answers[]{
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
         // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"", "", "", "", "", "", "", ""},
-        {"", "", "", "", "", "", "", ""},
+        {"d", "ffr"},
+        {"d", "d", "r"},
     };
 
     // ---- 不要修改以下代码 ----
 
-    for (auto i = 0; i < 3; ++i) {
+    for (unsigned i = 0; i < 3; ++i) {
         ASSERT(problems[i].size() == answers[i].size(), "wrong size");
-        for (auto j = 0; j < problems[i].size(); ++j) {
+        for (unsigned j = 0; j < problems[i].size(); ++j) {
             ASSERT(std::strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
         }
     }
